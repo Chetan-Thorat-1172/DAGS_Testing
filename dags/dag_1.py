5972878ce395972b7eb64d117738f716d20c5218
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-05-08 09:13:10 +0530

    Update dag_1.py

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -14,7 +14,7 @@ def marker(name, **context):
 with DAG(
     dag_id="dag_1",
     schedule_interval="30 18 * * *",
-    start_date=datetime(2026, 5, 7),
+    start_date=datetime(2026, 5, 8),
     catchup=False,
     default_args={"snowflake_conn_id": "harsh_conn", "retries": 1, "retry_delay_seconds": 5},
     description="Load test DAG 1 with 17 tasks and branch-heavy Snowflake flow",
