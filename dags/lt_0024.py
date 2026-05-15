3f134498188e41381435f59778865a72310e792e
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-05-15 18:43:03 +0530

    Add 50 load test DAGs (5 tasks each, schedule 52 18 * * *)

---
+++ dags/lt_0024.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, PythonOperator
+
+with DAG(
+    dag_id="lt_0024",
+    schedule_interval="52 18 * * *",
+    start_date=datetime(2026, 5, 15),
+    catchup=False,
+    max_active_runs=3,
+    description="Load test: 5-task python chain",
+) as dag:
+    t1 = PythonOperator(task_id="step_1", python_callable="import time; time.sleep(1); print('step_1_done')")
+    t2 = PythonOperator(task_id="step_2", python_callable="import time; time.sleep(1); print('step_2_done')")
+    t3 = PythonOperator(task_id="step_3", python_callable="import time; time.sleep(1); print('step_3_done')")
+    t4 = PythonOperator(task_id="step_4", python_callable="import time; time.sleep(1); print('step_4_done')")
+    t5 = PythonOperator(task_id="step_5", python_callable="import time; time.sleep(1); print('step_5_done')")
+    t1 >> t2 >> t3 >> t4 >> t5
---
+++ dags/lt_0035.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator
+
+with DAG(
+    dag_id="lt_0035",
+    schedule_interval="52 18 * * *",
+    start_date=datetime(2026, 5, 15),
+    catchup=False,
+    max_active_runs=3,
+    description="Load test: 5-task bash chain",
+) as dag:
+    t1 = BashOperator(task_id="step_1", bash_command="sleep 1 && echo step_1_done")
+    t2 = BashOperator(task_id="step_2", bash_command="sleep 1 && echo step_2_done")
+    t3 = BashOperator(task_id="step_3", bash_command="sleep 1 && echo step_3_done")
+    t4 = BashOperator(task_id="step_4", bash_command="sleep 1 && echo step_4_done")
+    t5 = BashOperator(task_id="step_5", bash_command="sleep 1 && echo step_5_done")
+    t1 >> t2 >> t3 >> t4 >> t5
