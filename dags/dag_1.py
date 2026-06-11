738327e3822611518ec463d50b7b697cf8cdf90b
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-11 14:50:42 +0530

    Update dag_1.py

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 with DAG(
     dag_id="smoke_test_bash",
-    schedule_interval="52 12 * * *",
+    schedule_interval="0 15 * * *",
     start_date=datetime(2026, 6, 9),
     catchup=False,
     description="Single-task smoke test for deployment verification",
