b2b37255c3d8a2c0b2bfaa7140c9734367b5da6a
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-12 17:17:31 +0530

    Create AMS_EDW_DATALOAD.py

---
+++ dags/AMS_EDW_DATALOAD.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, PythonOperator, SnowflakeOperator
+
+with DAG(
+    dag_id="AMS_EDW_DATALOAD",
+    schedule_interval=None,
+    start_date=datetime(2026, 6, 12),
+    catchup=False,
+    default_args={"snowflake_conn_id": "svc_pi_flow_conn", "retries": 1, "retry_delay_seconds": 5},
+    description="AMS Daily EDW Data Load",
+) as dag:
+
+    t01 = SnowflakeOperator(task_id="TS_EXECUTE_AMS_DBT_PR", sql="EXECUTE DBT PROJECT TESTING.PI_FLOW_LOAD_TEST.AMS_DBT_PROJECT ARGS = 'run';")
+
+    # Dependencies:  t01
+    t01
