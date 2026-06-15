48b682612368f3a9c1add326d8336cd0b2858d8e
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-15 14:33:51 +0530

    Create quickstart.py

---
+++ dags/quickstart.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from dag_parser.dynamic.operators import PythonOperator, BashOperator
+
+with DAG(
+    dag_id="hello_piflow",
+    schedule="@daily",
+    start_date=datetime(2026, 6, 15),
+    catchup=False,
+    tags=["tutorial"],
+) as dag:
+
+    def say_hello(**context):
+        print("Hello from", context["ds"])
+        return {"greeted": True}
+
+    hello = PythonOperator(task_id="hello", python_callable=say_hello)
+    done = BashOperator(task_id="done", bash_command="echo finished")
+
+    hello >> done
