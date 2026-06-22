cf10613de2316ef3d31464596e9da8d9a45c818b
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-22 16:59:03 +0530

    Update and rename 03_cron_daily.py to test_catchup.py

--- dags/test_catchup.py
+++ dags/test_catchup.py
@@ -3,9 +3,9 @@ from datetime import datetime
 from dag_parser.dynamic.dag_context import DAG, BashOperator
 
 with DAG(
-    dag_id="cron_daily",
-    schedule_interval="@daily",
-    start_date=datetime(2026, 6, 18, 0, 0),
+    dag_id="test_catchup",
+    schedule_interval="* * * * *",
+    start_date=datetime(2026, 6, 22),
     catchup=False,
 ) as dag:
     BashOperator(task_id="daily_task", bash_command="echo 'Daily run at {{ .DS }}'")
