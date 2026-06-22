80d91c47ae3f369691e4f2ac2a807be252c48445
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-22 17:49:40 +0530

    Update ver_midflight_test.py

--- dags/ver_midflight_test.py
+++ dags/ver_midflight_test.py
@@ -5,9 +5,10 @@ with DAG(
     dag_id="ver_midflight_test",
     schedule_interval="@once",
     start_date=datetime(2026, 6, 22),
-    catchup=True,
+    catchup=False,
 ) as dag:
-    t1 = BashOperator(task_id="start", bash_command="echo 'V1 started'")
-    t2 = BashOperator(task_id="slow", bash_command="sleep 360 && echo 'V1 slow done'")
-    t3 = BashOperator(task_id="finish", bash_command="echo 'V1 finish'")
-    t1 >> t2 >> t3
+    t1 = BashOperator(task_id="start", bash_command="echo 'V2 started'")
+    t2 = BashOperator(task_id="slow", bash_command="sleep 360 && echo 'V2 slow done'")
+    t4 = BashOperator(task_id="new_task", bash_command="echo 'V2 new task'")
+    
+    t1 >> t2 >> t4
