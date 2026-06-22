3b7c11b7385beed908abc6c6f276e83d3e2a9860
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-22 14:47:04 +0530

    Create test_hourly.py

---
+++ dags/test_hourly.py
@@ -0,0 +1,11 @@
+"""TEST 8: @hourly preset - creates runs once per hour"""
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator
+
+with DAG(
+    dag_id="test_hourly",
+    schedule_interval="@hourly",
+    start_date=datetime(2026, 6, 22),
+    catchup=True,
+) as dag:
+    BashOperator(task_id="hourly_task", bash_command="echo 'Hourly run at {{ .TS }}'")
