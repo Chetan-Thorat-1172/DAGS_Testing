ce088ffda9e89b7dd58c089c9197048ba86cd215
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-22 17:45:20 +0530

    Create ver_midflight_test.py

---
+++ dags/ver_midflight_test.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator
+
+with DAG(
+    dag_id="ver_midflight_test",
+    schedule_interval="@once",
+    start_date=datetime(2026, 6, 22),
+    catchup=True,
+) as dag:
+    t1 = BashOperator(task_id="start", bash_command="echo 'V1 started'")
+    t2 = BashOperator(task_id="slow", bash_command="sleep 360 && echo 'V1 slow done'")
+    t3 = BashOperator(task_id="finish", bash_command="echo 'V1 finish'")
+    t1 >> t2 >> t3
