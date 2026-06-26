e6e961057fd2140263768bfa59537b1fbe3b11d9
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-26 14:45:18 +0530

    Create test_http_sensor.py

---
+++ dags/test_http_sensor.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator, HttpSensor
+
+
+with DAG(
+    dag_id="test_http_sensor",
+    schedule_interval=None,
+    start_date=datetime(2026, 6, 26),
+    catchup=False,
+) as dag:
+
+    wait_for_api = HttpSensor(
+        task_id="wait_for_api",
+        endpoint="https://httpstat.us/200",
+        method="GET",
+        poke_interval=10,
+        timeout=60,
+    )
+
+    done = BashOperator(
+        task_id="done",
+        bash_command="echo 'HttpSensor condition met!'",
+    )
+
+    wait_for_api >> done
