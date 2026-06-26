4eaed195aba1d82a3e2ac1f2eeccf9de01fb1f0a
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-06-26 14:21:39 +0530

    Create test_sql_sensor.py

---
+++ dags/test_sql_sensor.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator, SqlSensor
+
+
+with DAG(
+    dag_id="test_sql_sensor",
+    schedule_interval=None,
+    start_date=datetime(2026, 6, 26),
+    catchup=False,
+) as dag:
+
+    wait_for_data = SqlSensor(
+        task_id="wait_for_flag",
+        sql="SELECT 1 FROM variable WHERE key = 'sensor_test_flag'",
+        poke_interval=10,
+        timeout=120,
+        mode="poke",
+    )
+
+    done = BashOperator(
+        task_id="done",
+        bash_command="echo 'SqlSensor condition met!'",
+    )
+
+    wait_for_data >> done
