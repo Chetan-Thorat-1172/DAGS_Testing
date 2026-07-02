74863811f4171e66fca4bcae3d1308f34e94c426
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-07-02 14:44:59 +0530

    Create test_daily_partition.py

---
+++ dags/test_daily_partition.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from dag_parser.dynamic.dag_context import DAG, BashOperator, DailyPartition
+
+
+with DAG(
+    dag_id="test_daily_partition",
+    schedule="@daily",
+    start_date=datetime(2026, 7, 1),
+    catchup=False,
+    partitions=DailyPartition(),
+) as dag:
+    BashOperator(
+        task_id="run",
+        bash_command="echo daily partition run",
+    )
