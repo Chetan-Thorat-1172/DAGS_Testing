688c9c7e386e440a8cc0c65e5c7ca83633aef086
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-07-02 16:48:38 +0530

    Create test_zombie.py

---
+++ dags/test_zombie.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator
+
+with DAG(
+    dag_id="test_zombie",
+    schedule_interval=None,
+    start_date=datetime(2026, 7, 2),
+    catchup=False,
+) as dag:
+
+    t_long = BashOperator(
+        task_id="t_long",
+        bash_command="sleep 90",
+        retries=1,
+        retry_delay_seconds=5,
+    )
