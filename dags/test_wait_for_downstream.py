bb6f6c3773affc8a1d9ef0439b6403503b371171
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-07-09 16:44:42 +0530

    Create test_wait_for_downstream.py

---
+++ dags/test_wait_for_downstream.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator
+
+with DAG(
+    dag_id="test_wait_for_downstream",
+    schedule_interval="@daily",
+    start_date=datetime(2026, 7, 5),
+    catchup=True,
+) as dag:
+
+    extract = BashOperator(
+        task_id="extract",
+        bash_command="echo extract",
+        wait_for_downstream=True,
+    )
+
+    transform = BashOperator(
+        task_id="transform",
+        bash_command="echo transform",
+    )
+
+    load = BashOperator(
+        task_id="load",
+        bash_command="echo load",
+    )
+
+    extract >> transform >> load
