ca8ad9fe7cbbb661c323dceaa1cbdcd2182738cc
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-07-13 11:47:58 +0530

    Update start_date in test_pools2 DAG

---
+++ dags/test_pools2.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator
+
+with DAG(
+    dag_id="test_pools2",
+    schedule_interval="@daily",
+    start_date=datetime(2026, 7, 13),
+    catchup=True,
+) as dag:
+    t1 = BashOperator(
+        task_id="t1",
+        bash_command="sleep 60",
+        pool="test_pool",
+    )
+
+    t2 = BashOperator(
+        task_id="t2",
+        bash_command="sleep 60",
+        pool="test_pool",
+    )
+
+    t3 = BashOperator(
+        task_id="t3",
+        bash_command="sleep 60",
+        pool="test_pool",
+    )
+
+    t4 = BashOperator(
+        task_id="t4",
+        bash_command="sleep 60",
+        pool="test_pool",
+    )
