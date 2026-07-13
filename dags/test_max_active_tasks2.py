33565d29afcaaac3878406f7cb0c097356311a95
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-07-13 12:25:25 +0530

    Update test_max_active_tasks2.py

--- dags/test_max_active_tasks2.py
+++ dags/test_max_active_tasks2.py
@@ -3,7 +3,7 @@ from dag_parser.dynamic.dag_context import DAG, BashOperator
 
 with DAG(
     dag_id="test_max_active_tasks2",
-    schedule_interval="@daily",
+    schedule_interval=None,
     start_date=datetime(2026, 7, 10),
     catchup=True,
     max_active_tasks=2,
