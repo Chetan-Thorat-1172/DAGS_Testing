6595a7c33fc48dbeb7fdd29c3836fd733bce5bf8
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-07-14 09:42:42 +0530

    Update test_sla.py

--- dags/test_sla.py
+++ dags/test_sla.py
@@ -2,10 +2,10 @@ from datetime import datetime
 
 with DAG(
     dag_id="test_sla",
-    schedule_interval=None,
+    schedule_interval="@daily",
     start_date=datetime(2026, 7, 13),
     expected_duration_seconds=10,  # DAG-level SLA: 10 seconds
-    catchup=False,
+    catchup=True,
     on_sla_miss_callback=SmtpNotifier(
         to=["Chetan.Thorat@Pibythree.com"],
         subject="SLA MISS: DAG {{ dag_id }}",
