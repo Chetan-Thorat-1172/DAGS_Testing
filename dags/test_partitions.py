e5e4647743d2583e3c9ed2ce94792e13950a7443
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-07-14 11:59:45 +0530

    Create test_partitions.py

---
+++ dags/test_partitions.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from dag_parser.dynamic.dag_context import (
+    DAG,
+    BashOperator,
+    DailyPartition,
+)
+
+
+with DAG(
+    dag_id="test_partitions",
+    schedule_interval="@daily",
+    start_date=datetime(2026, 7, 10),
+    catchup=True,
+    partitions=DailyPartition(),
+) as dag:
+
+    t1 = BashOperator(
+        task_id="process_partition",
+        bash_command="echo processing partition",
+    )
