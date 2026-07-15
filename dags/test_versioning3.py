cf0483a8d86c34ef9dde3e311199c4bd58ad8363
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-07-15 14:27:42 +0530

    Update test_versioning3.py

--- dags/test_versioning3.py
+++ dags/test_versioning3.py
@@ -1,26 +1,28 @@
- from datetime import datetime
-    from dag_parser.dynamic.dag_context import DAG, BashOperator
+from datetime import datetime
 
-    with DAG(
-        dag_id="test_versioning3",
-        schedule_interval=None,
-        start_date=datetime(2026, 7, 15),
-        catchup=False,
-    ) as dag:
+from dag_parser.dynamic.dag_context import DAG, BashOperator
 
-        t1 = BashOperator(
-            task_id="step_1",
-            bash_command="echo step1",
-        )
 
-        t2 = BashOperator(
-            task_id="step_2",
-            bash_command="echo step2 && sleep 80",
-        )
+with DAG(
+    dag_id="test_versioning3",
+    schedule_interval=None,
+    start_date=datetime(2026, 7, 15),
+    catchup=False,
+) as dag:
 
-        t3 = BashOperator(
-            task_id="stop",
-            bash_command="echo stop",
-        )
+    t1 = BashOperator(
+        task_id="step_1",
+        bash_command="echo step1",
+    )
 
-        t1 >> t2 >> t3
+    t2 = BashOperator(
+        task_id="step_2",
+        bash_command="echo step2 && sleep 80",
+    )
+
+    t3 = BashOperator(
+        task_id="stop",
+        bash_command="echo stop",
+    )
+
+    t1 >> t2 >> t3
