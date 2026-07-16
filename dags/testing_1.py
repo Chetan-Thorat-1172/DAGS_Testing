c68a9e73eeb2ab22e71cd6bce12320a589e42385
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-07-16 13:12:19 +0530

    s

---
+++ dags/testing_1.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from dag_parser.dynamic.dag_context import DAG, BashOperator
+
+with DAG(
+    dag_id="load_test_scal",
+    schedule_interval=None,
+    start_date=datetime(2026, 7, 2),
+    catchup=False,
+    max_active_tasks=200,
+) as dag:
+    tasks = []
+    for i in range(1, 301):
+        t = BashOperator(
+            task_id=f"load_task_{i}",
+            bash_command=f"echo 'task {i} start'; sleep 8; echo 'task {i} done'",
+        )
+        tasks.append(t)
