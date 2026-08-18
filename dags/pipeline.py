13feb8fd67e282f9ada0caf7a501e4bf58f981e3
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-08-18 14:47:48 +0530

    Rename DAG from 'xyz' to 'pipeline'

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 with DAG(
-    dag_id="xyz",
+    dag_id="pipeline",
     schedule_interval=None,
     start_date=datetime(2026, 7, 13),
     expected_duration_seconds=10,  # DAG-level SLA: 10 seconds
     catchup=True,
-    tags=["p_viewer","p_tester"],
+    tags=["p_viewer", "p_tester"],
     on_sla_miss_callback=SmtpNotifier(
         to=["Chetan.Thorat@Pibythree.com"],
         subject="SLA MISS: DAG {{ dag_id }}",
