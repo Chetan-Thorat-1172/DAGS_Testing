8a24c855a7cdcf80f3c1c982373353141aa8df3a
harshvardhan-27afk <harshavardhan.sankpal@pibythree.com>
2026-08-20 09:32:33 +0530

    testing nesting

---
+++ dags/DATA_TEAM/Project_A/dag_1.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+with DAG(
+    dag_id="dag_1",
+    schedule_interval=None,
+    start_date=datetime(2026, 7, 13),
+    expected_duration_seconds=10,  # DAG-level SLA: 10 seconds
+    catchup=True,
+    on_sla_miss_callback=SmtpNotifier(
+        to=["Chetan.Thorat@Pibythree.com"],
+        subject="SLA MISS: DAG {{ dag_id }}",
+    ),
+) as dag:
+
+    t_slow = BashOperator(
+        task_id="t_slow",
+        bash_command="sleep 30",  # Exceeds the 10-second SLA
+    )
--- dags/DATA_TEAM/Project_A/sub-project/sub-project_dag.py
+++ dags/DATA_TEAM/Project_A/sub-project/sub-project_dag.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 
 with DAG(
-    dag_id="project_A_dag1",
+    dag_id="sub-project_DAG",
     schedule_interval=None,
     start_date=datetime(2026, 7, 13),
     expected_duration_seconds=10,  # DAG-level SLA: 10 seconds
     catchup=True,
-    tags=["p_viewer", "p_tester"],
     on_sla_miss_callback=SmtpNotifier(
         to=["Chetan.Thorat@Pibythree.com"],
         subject="SLA MISS: DAG {{ dag_id }}",
