3e85885eaeebe5596ec7c4fdf4291d6206457b52
Chetan-Thorat-1172 <Chetan.Thorat@Pibythree.com>
2026-09-10 17:51:16 +0530

    Update email_notification_dag.py

--- dags/email_notification_dag.py
+++ dags/email_notification_dag.py
@@ -10,7 +10,7 @@ Manually triggered only (schedule=None) while getting comfortable with PI-Flow.
 
 from datetime import datetime
 
-from dag_parser.dynamic.dag_context import DAG, SnowflakeOperator
+from dag_parser.dynamic.dag_context import DAG, SQLExecuteQueryOperator
 
 RECIPIENT_EMAIL = "bharat.rao@pibythree.com"  # <-- replace with real recipient
 CONN="snowflake_conn"
